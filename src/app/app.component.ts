1535de765220925427fec2582fc1e1a6a6dc43e5
snopbear <snopbear@gmail>
2024-06-29 16:25:58 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'conduit-ngrx-16-v';
+}
